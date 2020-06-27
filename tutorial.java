ab663295f891be200729abd36d520d82d5eb0d1b
grvk28 <40272398+grvk28@users.noreply.github.com>
2020-06-27 11:28:00 +0530

    Add files via upload

---
+++ tutorial.java
@@ -0,0 +1,23 @@
+package javatut;
+
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+public class tutorial {
+	
+	public static void main(String[] args)
+	{
+		Model m= ModelFactory.createDefaultModel();
+		String NS = "http://example.com/test/";
+		
+		Resource r = m.createResource(NS + "r");
+		Property p = m.createProperty(NS+"p");
+		
+		r.addProperty(p, "hello world",XSDDatatype.XSDstring );
+		m.write(System.out, "Turtle");
+	}
+	
+}  
